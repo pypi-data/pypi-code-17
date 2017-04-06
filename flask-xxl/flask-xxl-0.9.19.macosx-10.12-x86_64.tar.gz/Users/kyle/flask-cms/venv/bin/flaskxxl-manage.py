79ac624dc5e8a97d7e9d28dcc7b796ea891d1aa5
Tom Forbes <tom@tomforb.es>
2017-04-06 22:28:36 +0000

    {"name":"flask-xxl","version":"0.9.19","file":"flask-xxl-0.9.19.macosx-10.12-x86_64.tar.gz","path":"flask-xxl/flask-xxl-0.9.19.macosx-10.12-x86_64.tar.gz"}

---
+++ flask-xxl/flask-xxl-0.9.19.macosx-10.12-x86_64.tar.gz/Users/kyle/flask-cms/venv/bin/flaskxxl-manage.py
@@ -0,0 +1,12 @@
+#!/Users/kyle/flask-cms/venv/bin/python
+# EASY-INSTALL-ENTRY-SCRIPT: 'flask-xxl==0.9.19','console_scripts','flaskxxl-manage.py'
+__requires__ = 'flask-xxl==0.9.19'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('flask-xxl==0.9.19', 'console_scripts', 'flaskxxl-manage.py')()
+    )
